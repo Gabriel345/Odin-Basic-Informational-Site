f157afd72592c43c96437d746297b4e981a9353f
Gabriel345 <gabrielmzenerato242@gmail.com>
2024-03-15 16:13:20 -0300

    rotas atualizadas para express criado pasta 'public' e pasta 'css'
    arquivo style.css criado para estilização

--- App.js
+++ App.js
@@ -1,78 +1,39 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 8080;
 
-const port = 8080;
+const publicDirectoryPath = path.join(__dirname, 'public');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
 
-        fs.readFile('./html/index.html', (err, data) => {
-            if (err) {
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Erro interno no servidor');
+app.use(express.static(publicDirectoryPath));
 
-            } else {
-                res.statusCode = 200
-                res.setHeader('Content-Type', 'text/html');
-                res.end(data);
-            }
-        });
-    }else if (req.url === '/about') {
 
-        fs.readFile('./html/about.html', (err, data) => {
-            if (err) {
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Erro interno no servidor');
-
-            } else {
-                res.statusCode = 200
-                res.setHeader('Content-Type', 'text/html');
-                res.end(data);
-            }
-        }); 
+app.get('/', (req, res) => {
     
-    }else if (req.url === '/contact-me') {
-
-        fs.readFile('./html/contact-me.html', (err, data) => {
-            if (err) {
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Erro interno no servidor');
-
-            } else {
-                res.statusCode = 200
-                res.setHeader('Content-Type', 'text/html');
-                res.end(data);
-            }
-        }); 
+    res.sendFile(path.join(publicDirectoryPath, 'index.html'));
+});
+app.get('/about', (req, res) => {
     
-    }
-    else {
-
-        fs.readFile('./html/404.html', (err, data) => {
-            if (err) {
-
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Erro interno no servidor')
-            }else{
-                res.statusCode = 200;
-                res.setHeader('Content-Type','text/html');
-                res.end(data);
-            }
-        })
-    }
-
-
-
-    
-
-
-
+    res.sendFile(path.join(publicDirectoryPath, 'about.html'));
 });
 
-server.listen(port, () => {
-    console.log(`Sevidor rodando em http://localhost:${port}`);
-})
+app.get('/contact-me', (req, res) => {
+   
+    res.sendFile(path.join(publicDirectoryPath, 'contact-me.html'));
+});
+app.use((req,res)=>{
+    res.status(404).sendFile(path.join(publicDirectoryPath,'404.html'));
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo deu errado!');
+});
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor está rodando em http://localhost:${PORT}`);
+});
